dca1e21474fce21a28a9a1a1c6ec66e4142c97de
NHAT KHANH LINH Nguyen <546682@student.fontys.nl>
2025-01-23 13:56:50 +0100

    Complete Responsive Carousel

--- customer-interface/src/App.jsx
+++ customer-interface/src/App.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
+import Slider from 'react-slick';
 import Header from './components/Header';
 import TopNavigation from './components/TopNavigation';
+import ResponsiveCarousel from './components/ResponsiveCarousel';
+
 // import { BrowserRouter } from 'react-router-dom';
 
 
 import './styles/Header.scss';
 import './styles/TopNavigation.scss';
+import './styles/ResponsiveCarousel.scss';
 
 function App() {
   return (
     <>
       <Header/>
       <TopNavigation/>
+      <ResponsiveCarousel/>
     </>
   )
 }
---
+++ customer-interface/src/components/ResponsiveCarousel.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import '../styles/ResponsiveCarousel.scss';
+
+import res1 from '../assets/res1.png';
+
+class ResponsiveCarousel extends React.Component {
+    render (){
+        const settings = {
+            dots: true,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: true,
+        };
+        return (
+            <div className="carousel-background">
+                <div className="carousel-container">
+                    <Slider {...settings}>
+                        <div>
+                            <img src={res1} alt="Slide 1" />                
+                        </div>
+                        <div>
+                            <img src={res1} alt="Slide 1" />                
+                        </div>
+                        <div>
+                            <img src={res1} alt="Slide 1" />   
+                        </div>
+                    </Slider>
+                    {/* <button className="previous">&lt;</button>
+                    <button className="next">&gt;</button> */}
+                </div>
+            </div>
+           
+        );
+    }
+}
+
+export default ResponsiveCarousel;
