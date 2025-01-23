31f8edcf5ec148c0e93ea32057592fc0c94afdc9
NHAT KHANH LINH Nguyen <546682@student.fontys.nl>
2025-01-23 12:06:25 +0100

    Complete top navigation

--- customer-interface/src/App.jsx
+++ customer-interface/src/App.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import Header from './components/Header';
+import TopNavigation from './components/TopNavigation';
 // import { BrowserRouter } from 'react-router-dom';
 
 
 import './styles/Header.scss';
+import './styles/TopNavigation.scss';
 
 function App() {
   return (
     <>
       <Header/>
-      <div >Hello World</div>
+      <TopNavigation/>
     </>
   )
 }
--- customer-interface/src/components/TopNavigation.jsx
+++ customer-interface/src/components/TopNavigation.jsx
@@ -1,2 +1,26 @@
 import React from 'react';
+import '../styles/TopNavigation.scss';
+
+import logo from '../assets/logo.png'
  
+class TopNavigation extends React.Component {
+    render() {
+        return (
+            <div className="topnav">
+                <div class="topnav-left">
+                    <img src={logo} alt="Logo"/>
+                </div>
+                <div class="topnav-right">
+                    <a class="active" href="#home">Trang chủ</a>
+                    <a href="#news">Giới thiệu</a>
+                    <a href="#contact">Doanh Nghiệp</a>
+                    <a href="#about">Sở hữu trí tuệ</a>
+                    <a href="#news">Dân sự</a>
+                    <a href="#contact">Hình sự</a>
+                    <a href="#about">Hành Chính</a>
+                </div>
+            </div>
+        )
+    }
+}
+ export default TopNavigation;
