e8777d5387df49e816bbad943208ec92c81f0fad
NHAT KHANH LINH Nguyen <546682@student.fontys.nl>
2025-01-23 15:09:22 +0100

    Complete half of AboutCompany component in HomePage

--- customer-interface/src/App.jsx
+++ customer-interface/src/App.jsx
@@ -3,13 +3,13 @@ import Slider from 'react-slick';
 import Header from './components/Header';
 import TopNavigation from './components/TopNavigation';
 import ResponsiveCarousel from './components/ResponsiveCarousel';
+import AboutCompany from './components/AboutCompany';
 
 // import { BrowserRouter } from 'react-router-dom';
-
-
 import './styles/Header.scss';
 import './styles/TopNavigation.scss';
 import './styles/ResponsiveCarousel.scss';
+import './styles/AboutCompany.scss';
 
 function App() {
   return (
@@ -17,6 +17,7 @@ function App() {
       <Header/>
       <TopNavigation/>
       <ResponsiveCarousel/>
+      <AboutCompany/>
     </>
   )
 }
---
+++ customer-interface/src/components/AboutCompany.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import '../styles/AboutCompany.scss';
+import pic from '../assets/about-company.png';
+
+class AboutCompany extends React.Component {
+    render() {
+        return (
+            <>
+                <div className="about-company">
+                    <div className="about-company-content">
+                        <h1 className="title">Công ty Luật Kim Ngọc</h1>
+                        <h className="description">
+                            Lorem ipsum dolor sit amet. Ut quas internos 33 beatae temporibus et molestiae quisquam non rerum inventore! Et voluptas obcaecati in rerum animi ut odit facilis et assumenda nesciunt qui recusandae quae id voluptates pariatur eos molestiae voluptas. Ut temporibus quas quo consequuntur magni ut numquam doloremque.
+                        </h>
+                        <br></br>
+                        <button className="view-more-introduction">
+                            Tổng quan công ty Luật Kim Ngọc
+                        </button>
+                    </div>
+                    <div className="about-company-pic">
+                        <div className="pic-background">
+                            <img src ={pic}/>
+                        </div>
+                    </div>
+                </div>
+            </>
+        )
+    }
+}
+
+export default AboutCompany;
