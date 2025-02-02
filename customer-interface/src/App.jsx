70d9f29f50ff13a5cff31db0116f498d2c1559d2
NHAT KHANH LINH Nguyen <546682@student.fontys.nl>
2025-02-02 14:52:53 +0100

    Add News add

--- customer-interface/src/App.jsx
+++ customer-interface/src/App.jsx
@@ -5,12 +5,14 @@ import TopNavigation from './components/TopNavigation';
 import ResponsiveCarousel from './components/ResponsiveCarousel';
 import AboutCompany from './components/AboutCompany';
 import OfferService from './components/OfferService';
+import News from './components/News';
 // import { BrowserRouter } from 'react-router-dom';
 import './styles/Header.scss';
 import './styles/TopNavigation.scss';
 import './styles/ResponsiveCarousel.scss';
 import './styles/AboutCompany.scss';
 import'./styles/OfferService.scss';
+import './styles/News.scss';
 
 function App() {
   return (
@@ -20,6 +22,7 @@ function App() {
       <ResponsiveCarousel/>
       <AboutCompany/>
       <OfferService/>
+      <News/>
     </>
   )
 }
