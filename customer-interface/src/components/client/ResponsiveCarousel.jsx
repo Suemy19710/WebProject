3549d4ff7929c34f3dc7e99ef15b8288c2f56d9b
NHAT KHANH LINH Nguyen <546682@student.fontys.nl>
2025-02-27 15:37:46 +0100

    add some pictures in responsive carousel

--- customer-interface/src/components/client/ResponsiveCarousel.jsx
+++ customer-interface/src/components/client/ResponsiveCarousel.jsx
@@ -3,8 +3,11 @@ import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import '../../styles/client/ResponsiveCarousel.scss';
-
 import res1 from '../../assets/res1.png';
+import res2 from '../../assets/res2.jpg';
+import res3 from '../../assets/res3.jpg';
+import res4 from '../../assets/res4.jpg';
+
 
 class ResponsiveCarousel extends React.Component {
     render (){
@@ -23,10 +26,13 @@ class ResponsiveCarousel extends React.Component {
                 <div className="carousel-container">
                     <Slider {...settings}>
                         <div>
-                            <img src={res1} alt="Slide 1" />                
+                            <img src={res3} alt="Slide 3" />                
                         </div>
                         <div>
-                            <img src={res1} alt="Slide 1" />                
+                            <img src={res2} alt="Slide 2" />                
+                        </div>
+                        <div>
+                            <img src={res4} alt="Slide 3" />   
                         </div>
                         <div>
                             <img src={res1} alt="Slide 1" />   
