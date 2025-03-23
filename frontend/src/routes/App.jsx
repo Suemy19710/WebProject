e5f3eb3137321c9ee19eae16ae3249b764d7810f
I546682 <546682@student.fontys.nl>
2025-03-23 21:47:29 +0100

    fix some design in admin page

--- frontend/src/routes/App.jsx
+++ frontend/src/routes/App.jsx
@@ -14,16 +14,15 @@ import SoHuuTriTue from '../pages/client/SoHuuTriTuePage';
 import LuatSuDetailPage from '../pages/client/Lawyer/LuatSuDetailPage'; 
 import DichVuPage from '../pages/client/DichVuPage'; 
 
-// import AdminNotificationCustomerPage from '../pages/admin/AdminNotificationCustomerPage';
-// import AdminDanSuPage from '../pages/admin/AdminDanSuPage';
-// import AdminHinhSuPage from '../pages/admin/AdminHinhSuPage';
-// import AdminHanhChinhPage from '../pages/admin/AdminHanhChinhPage';
-// import AdminSoHuuTriTuePage from '../pages/admin/AdminSoHuuTriTuePage';
-// import AdminPreview from '../pages/admin/AdminPreviewPage';
-// import AdminLogin from '../components/admin/AdminLogin';
-// import AdminDashboard from '../pages/admin/AdminDashboardPage';
-// import AdminNewsPage from '../pages/admin/AdminNewsPage';
-// import AdminHeader from '../components/admin/AdminHeader';
+import AdminNotificationCustomerPage from '../pages/admin/AdminNotificationCustomerPage';
+import AdminDanSuPage from '../pages/admin/AdminDanSuPage';
+import AdminHinhSuPage from '../pages/admin/AdminHinhSuPage';
+import AdminHanhChinhPage from '../pages/admin/AdminHanhChinhPage';
+import AdminSoHuuTriTuePage from '../pages/admin/AdminSoHuuTriTuePage';
+import AdminPreview from '../pages/admin/AdminPreviewPage';
+import AdminLogin from '../components/admin/AdminLogin';
+import AdminDashboard from '../pages/admin/AdminDashboardPage';
+import AdminNewsPage from '../pages/admin/AdminNewsPage';
 
 
 
@@ -48,16 +47,15 @@ function App() {
 
 
         {/* Admin Routes */}
-        {/* <Route path="/admin" element={<AdminLogin/>}/>
+        <Route path="/admin" element={<AdminLogin/>}/>
         <Route path="/admin/dashboard" element={<AdminDashboard/>}/>
         <Route path="/admin/thong-bao" element={<AdminNotificationCustomerPage/>}/>
         <Route path ="/admin/dan-su" element={<AdminDanSuPage/>}/>
         <Route path ="/admin/hinh-su" element={<AdminHinhSuPage/>}/>
         <Route path ="/admin/hanh-chinh" element={<AdminHanhChinhPage/>}/>
         <Route path ="/admin/so-huu-tri-tue" element={<AdminSoHuuTriTuePage/>}/>
-        {/* <Route path="/admin/post-news" element={<AdminPostNews/>}/> */}
-        {/* <Route path="/admin/xem-truoc" element={<AdminPreview/>}/>
-        <Route path="/admin/tin-tuc" element={<AdminNewsPage/>} />  */}
+        <Route path="/admin/xem-truoc" element={<AdminPreview/>}/>
+        <Route path="/admin/tin-tuc" element={<AdminNewsPage/>} /> 
 
         {/* <Route path='/admin/blog' element={<AdminBlog/>}/> */}
 
