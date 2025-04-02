f356e7620229c25a7c6f9e5e9a1b4668dec742ab
I546682 <546682@student.fontys.nl>
2025-04-02 10:10:10 +0200

    delete customer-interface folder

--- customer-interface/src/utils/slugUtils.js
+++
@@ -1,8 +0,0 @@
-export const createSlugTitle = (title) => {
-    return title
-        .toLowerCase()
-        .trim()
-        .replace(/\s+/g, '-') // Replace spaces with hyphens
-        .replace(/[^a-z0-9-]+/g, '') // Remove special characters
-        .replace(/--+/g, '-'); // Remove multiple hyphens
-};
