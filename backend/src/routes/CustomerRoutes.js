41b2c0fd1d1d94b2f43c3a4b3d9c57fa5ef2d185
I546682 <546682@student.fontys.nl>
2025-04-02 10:05:07 +0200

    delete folder backend

--- backend/src/routes/CustomerRoutes.js
+++
@@ -1,9 +0,0 @@
-
-const express = require('express');
-const router = express.Router();
-const customerController = require('../controller/CustomerController');
-
-router.post('/', customerController.registerCustomer);
-router.get('/', customerController.getAllCustomer);
-router.put('/:customerId/read-status', customerController.updateReadStatus);
-module.exports = router;
