481dbd2fa903d5cc715523d9ecd4159a8231aa83
I546682 <546682@student.fontys.nl>
2025-04-20 00:17:46 +0200

    fix hanhchinh backend

--- backend/src/services/HanhChinhService.js
+++ backend/src/services/HanhChinhService.js
@@ -1,6 +1,4 @@
 const HanhChinhModel = require('../models/HanhChinhModel');
-const redis = require('redis');
-const client = redis.createClient();
 
 const saveDocument = async (content) => {
     try {
@@ -12,56 +10,15 @@ const saveDocument = async (content) => {
         throw new Error('Error saving document');
     }
 };
-// const getAllDocuments = async () => {
-//     try {
-//         // Get the first document (most recent or first based on your needs)
-//         const document = await HanhChinhModel.findOne();
-//         return document ? [document] : []; // Return an array for frontend consistency
-//     } catch (error) {
-//         throw new Error('Error fetching document');
-//     }
-// };
 
 const getAllDocuments = async () => {
     try {
-        const cacheKey = 'hanh-chinh-documents';
-        
-        // Check if the data is in the cache
-        return new Promise((resolve, reject) => {
-            client.get(cacheKey, async (err, cachedData) => {
-                if (err) reject(err);
-                if (cachedData) {
-                    // If data is cached, return it immediately
-                    console.log('Returning cached data');
-                    return resolve(JSON.parse(cachedData));
-                } else {
-                    // Fetch from DB if not cached
-                    const documents = await HanhChinhModel.find();
-                    if (!documents || documents.length === 0) {
-                        return resolve([]);
-                    }
-                    
-                    // Cache the response for 1 hour (3600 seconds)
-                    client.setex(cacheKey, 3600, JSON.stringify(documents)); // Cache for 1 hour
-                    
-                    resolve(documents);
-                }
-            });
-        });
+        const documents = await HanhChinhModel.find();
+        return documents;
     } catch (error) {
         throw new Error('Error fetching documents');
     }
 };
-
-//oldest version
-// const getAllDocuments = async () => {
-//     try {
-//         const documents = await HanhChinhModel.find();
-//         return documents;
-//     } catch (error) {
-//         throw new Error('Error fetching documents');
-//     }
-// };
 
 const getDocumentById = async (id) => {
     try {
