f5f23ac182ba02bf341b9eb40dcc3fae30888b24
I546682 <546682@student.fontys.nl>
2025-04-20 01:53:46 +0200

    minor change

--- frontend/src/components/client/ReponsiveCarousel.jsx
+++ frontend/src/components/client/ReponsiveCarousel.jsx
@@ -3,9 +3,9 @@ import '../../styles/client/ReponsiveCarousel.scss';
 import carousel1 from '../../assets/carousel-1.png'; 
 import carousel2 from '../../assets/carousel-2.png'; 
 import carousel3 from '../../assets/carousel-3.png'; 
-import carousel4 from '../../assets/carousel-3.png'; 
-import carousel5 from '../../assets/carousel-2.png'; 
-import carousel6 from '../../assets/carousel-1.png'; 
+import carousel4 from '../../assets/carousel-4.png'; 
+import carousel5 from '../../assets/carousel-5.png'; 
+import carousel6 from '../../assets/carousel-6.png'; 
 
 
 const Carousel = () => {
